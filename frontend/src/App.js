8b7552eb80f9446c2bc5297fe31426caceb7122c
Rahul Mishra <rahul.mishra605@gmail.com>
2021-10-03 14:58:06 +0530

    Added the main users list page with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { routes } from './utils/constants';
+import { UsersList } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={routes.ROOT} component={UsersList} exact />        
+        <Route path={routes.ROOT} component={UsersList} exact />
       </Switch>
     </BrowserRouter>
   );
--- frontend/src/pages/UsersList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UsersList = () => {
-    return (
-        <div></div>
-    );
-}
-
-export default UsersList;
