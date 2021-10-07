c40f2b0737f39cfba9089376d1b2ffe43500a072
Rahul Mishra <rahul.mishra605@gmail.com>
2021-10-07 20:11:04 +0530

    added dummy data and pagination implementation

--- frontend/src/pages/UsersList/index.js
+++ frontend/src/pages/UsersList/index.js
@@ -1,9 +1,11 @@
-import React, {useCallback} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { Container, Table, Button, Pagination } from 'react-bootstrap'
 import { useParams, useHistory } from 'react-router-dom';
 
 import users from './users';
 const UsersList = () => {
+    const [paginationCount, setpaginationCount] = useState(1);
+    const [pageNo, setpageNo] = useState(1);
     const history = useHistory();
     const { page = 1 } = useParams();
     console.log("LLLLL", page);
@@ -11,11 +13,21 @@ const UsersList = () => {
 
     const pageClicked = (event) => {
         const pageNumber = parseInt(event.target.text);
+        setpageNo(pageNumber);
         history.push(`/${pageNumber}`);
     }
 
+    useEffect(() => {
+        if(users.length % 20 === 0 ) {
+            setpaginationCount(users.length / 20);
+        } else {
+            setpaginationCount(users.length / 20 + 1);
+        }
+        
+      setpageNo(page);
+      }, []);
     //use
-    for (let number = 1; number <= users.length / 2; number++) {
+    for (let number = 1; number <= paginationCount; number++) {
         console.log("AAAAA");
         pageItems.push(
             <Pagination.Item key={number} active={number === page} onClick={(event) => pageClicked(event)}>
@@ -25,15 +37,15 @@ const UsersList = () => {
     }
 
     const loadUserData = () => {
-        return users.map((user) => {
-            return (<tr key={user.id}>
+        return users.map((user, key) => {
+            return key >= (pageNo-1)*20 && key < pageNo*20 ? (<tr key={user.id}>
                 <td>{user.id}</td>
                 <td>{user.firstName}</td>
                 <td>{user.lastName}</td>
                 <td>{user.birthDate}</td>
                 <td><Button variant="link">Edit</Button></td>
                 <td><Button variant="link">Delete</Button></td>
-            </tr>)
+            </tr>) : null        
         })
     }
 
@@ -54,7 +66,6 @@ const UsersList = () => {
             </tbody>
         </Table>
     }
-
     return (
         <Container>
             <p>
--- frontend/src/pages/UsersList/users.js
+++ frontend/src/pages/UsersList/users.js
@@ -58,6 +58,366 @@ var users = [
         "firstName": "Steve",
         "lastName": "Rogers",
         "birthDate": "02/12/1912"
+    },
+    {
+        "id": 11,
+        "firstName": "Tony-p1",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 12,
+        "firstName": "Tony-p1",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 13,
+        "firstName": "Bruce-p1",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 14,
+        "firstName": "Bruce-p1",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 15,
+        "firstName": "Natasha-p1",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 16,
+        "firstName": "Natasha-p1",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 17,
+        "firstName": "Clint-p1",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 18,
+        "firstName": "Clint-p1",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 19,
+        "firstName": "Steve-p1",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 20,
+        "firstName": "Steve-p1",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 21,
+        "firstName": "Tony-p2",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 22,
+        "firstName": "Tony-p2",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 23,
+        "firstName": "Bruce-p2",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 24,
+        "firstName": "Bruce-p2",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 25,
+        "firstName": "Natasha-p2",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 26,
+        "firstName": "Natasha-p2",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 27,
+        "firstName": "Clint-p2",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 28,
+        "firstName": "Clint-p2",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 29,
+        "firstName": "Steve-p2",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 30,
+        "firstName": "Steve-p2",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 31,
+        "firstName": "Tony-p2",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 32,
+        "firstName": "Tony-p2",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 33,
+        "firstName": "Bruce-p2",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 34,
+        "firstName": "Bruce-p2",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 35,
+        "firstName": "Natasha-p2",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 36,
+        "firstName": "Natasha-p2",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 37,
+        "firstName": "Clint-p2",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 38,
+        "firstName": "Clint-p2",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 39,
+        "firstName": "Steve-p2",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 40,
+        "firstName": "Steve-p2",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 41,
+        "firstName": "Tony-p3",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 42,
+        "firstName": "Tony-p3",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 43,
+        "firstName": "Bruce-p3",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 44,
+        "firstName": "Bruce-p3",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 45,
+        "firstName": "Natasha-p3",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 46,
+        "firstName": "Natasha-p3",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 47,
+        "firstName": "Clint-p3",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 48,
+        "firstName": "Clint-p3",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 49,
+        "firstName": "Steve-p3",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 50,
+        "firstName": "Steve-p3",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 51,
+        "firstName": "Tony-p3",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 52,
+        "firstName": "Tony-p3",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 53,
+        "firstName": "Bruce-p3",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 54,
+        "firstName": "Bruce-p3",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 55,
+        "firstName": "Natasha-p3",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 56,
+        "firstName": "Natasha-p3",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 57,
+        "firstName": "Clint-p3",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 58,
+        "firstName": "Clint-p3",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 59,
+        "firstName": "Steve-p3",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 60,
+        "firstName": "Steve-p3",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 61,
+        "firstName": "Tony-p4",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 62,
+        "firstName": "Tony-p4",
+        "lastName": "Stark",
+        "birthDate": "02/12/1967"
+    },
+    {
+        "id": 63,
+        "firstName": "Bruce-p4",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 64,
+        "firstName": "Bruce-p4",
+        "lastName": "Banner",
+        "birthDate": "07/12/1977"
+    },
+    {
+        "id": 65,
+        "firstName": "Natasha-p4",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 66,
+        "firstName": "Natasha-p4",
+        "lastName": "Romanoff",
+        "birthDate": "09/12/1986"
+    },
+    {
+        "id": 67,
+        "firstName": "Clint-p4",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 68,
+        "firstName": "Clint-p4",
+        "lastName": "Barton",
+        "birthDate": "06/12/1983"
+    },
+    {
+        "id": 69,
+        "firstName": "Steve-p4",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
+    },
+    {
+        "id": 70,
+        "firstName": "Steve-p4",
+        "lastName": "Rogers",
+        "birthDate": "02/12/1912"
     }
 ];
 
